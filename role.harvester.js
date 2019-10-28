124f60fca4a1a6d052450e6a7fb7cabf35631765
Chris Connelley <chris.connelley@divvypay.com>
2019-10-27 18:57:04 -0600

    Added deposit resource

--- role.harvester.js
+++ role.harvester.js
@@ -8,7 +8,7 @@ var roleHarvester = {
       font: '10px',
       stroke: '#FFFFFF'
     })
-  },  
+  },
   doesAnotherCreepWantResource: function (creepAsking, resource) {
     for (var nameCreep in Game.creeps) {
       if (nameCreep === creepAsking) continue;
@@ -28,11 +28,48 @@ var roleHarvester = {
       align: 'left'
     });
   },
+  depositResource(creep) {
+    containers = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+      filter: (structure) => {
+        return (structure.structureType == STRUCTURE_CONTAINER && structure.store.getFreeCapacity() > 0);
+      }
+    });
+
+
+    var idsInCreepStore = Object.keys(creep.store);
+
+    // deposit all in container 
+    var resultTransfer = creep.transfer(containers[0], idsInCreepStore[0]);
+    if (resultTransfer !== 0 && resultTransfer !== ERR_NOT_IN_RANGE) {
+      console.log("resultTransfer (" + creep + ") to (" + containers[0] + "): " + util.errorCodeToDisplay(resultTransfer));
+    }
+    if (resultTransfer == ERR_NOT_IN_RANGE) {
+      creep.moveTo(containers[0], {
+        visualizePathStyle: {
+          stroke: '#ffffff'
+        }
+      });
+    }
+    // move to container if the container isn't close enough
+
+  },
   run: function (creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
-      var closest_energy = shared.findClosestEnergy(creep);  
+    this.displayBadge(creep);
+    // This is the deciding logic for the harvester right now
+
+    var idsInCreepStore = Object.keys(creep.store);
+
+    if ((idsInCreepStore.length == 1 && idsInCreepStore[0] !== 'energy') || idsInCreepStore.length > 1) {
+      console.log("Creep (" + creep + ") has stores of " + idsInCreepStore);
+      this.depositResource(creep);
+      return OK;
+    }
+
+    // If the creep still has capacity, collect energy
+    if (creep.store.getUsedCapacity() < creep.store.getCapacity()) {
+      var closest_energy = shared.findClosestEnergy(creep);
       var resultGather = shared.gatherEnergy(creep, closest_energy);
-    
+
       if (resultGather == ERR_NOT_IN_RANGE) {
         creep.moveTo(closest_energy, {
           visualizePathStyle: {
@@ -48,30 +85,37 @@ var roleHarvester = {
             structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
         }
       });
+
       var closest_target = null;
 
+      // TODO: for each target, find the closest
       targets.forEach((target) => {
         if (closest_target === null) {
           closest_target = target;
         }
       });
 
+      // Targets.length would be 0 if all of the other structures are full
+      //   So, instead deposit it in a container.
       if (targets.length == 0) {
-        console.log("no container targets");
+        // console.log("Spawn, extensions, and tower are full. Looking for containers.");
         targets = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
           filter: (structure) => {
-            return (structure.structureType == STRUCTURE_CONTAINER);
+            return (structure.structureType == STRUCTURE_CONTAINER && structure.store.getFreeCapacity() > 0);
           }
         });
 
+        if (targets.length === 0) {
+          console.log("All containers are full!");
+        }
         closest_target = targets[0];
       }
 
-      if (targets.length > 0) {
+      if (closest_target) {
         var resultTransfer = creep.transfer(closest_target, RESOURCE_ENERGY);
         if (resultTransfer !== 0 && resultTransfer !== ERR_NOT_IN_RANGE) {
           console.log("resultTransfer (" + creep + ") to (" + closest_target + "): " + util.errorCodeToDisplay(resultTransfer));
-        } 
+        }
         if (resultTransfer == ERR_NOT_IN_RANGE) {
           creep.moveTo(closest_target, {
             visualizePathStyle: {
@@ -82,7 +126,6 @@ var roleHarvester = {
       }
     }
 
-    this.displayBadge(creep);
   }
 };
 

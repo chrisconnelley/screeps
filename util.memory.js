7d322f914c5afd98768b3a3e512eed4de04f05d6
Chris Connelley <chris.connelley@divvypay.com>
2019-10-29 10:18:58 -0600

    Memory updates

--- util.memory.js
+++ util.memory.js
@@ -2,6 +2,11 @@ var utilMemory = {
   forget: function(creep, keyMemory) {
     delete creep.ummemory[keyMemory];
   },
+  getArray: function(creep, keyMemory) {
+    var arrayMemory = creep.memory.um[keyMemory];
+
+    return arrayMemory;
+  },
   getObject: function(creep, keyMemory) {
     var object = Game.getObjectById(creep.memory.um[keyMemory]);
     
@@ -10,6 +15,9 @@ var utilMemory = {
     };
 
     return object;
+  },
+  getBoolean: function(creep, keyMemory) {
+    return creep.memory.um[keyMemory];
   },
   getInt: function(creep, keyMemory) {
     return parseInt(creep.memory.um[keyMemory]);
@@ -22,14 +30,56 @@ var utilMemory = {
       creep.memory.um = {};
     };
 
-    return creep.memory.um[keyMemory] !== null;
+    var valueMemory = creep.memory.um[keyMemory];
+
+    // console.log("hasMemory: " + creep + " " + keyMemory + ": " + valueMemory + " " + (valueMemory !== undefined));
+    return valueMemory !== undefined;
   },
   remember: function(creep, keyMemory, valueMemory) {
+    // console.log("remember " + creep + " [" + keyMemory + ":" + valueMemory + "]");
     if (creep.memory.um === undefined) { 
       creep.memory.um = {};
     }
 
     creep.memory.um[keyMemory] = valueMemory;
+  },
+  rememberInArray(creep, keyMemory, valueMemory) {
+    // console.log("rememberInArray " + creep + " [" + keyMemory + ":" + valueMemory + "]");
+    if (creep.memory.um === undefined) { 
+      creep.memory.um = {};
+    }
+
+    if (creep.memory.um[keyMemory] === undefined) {
+      creep.memory.um[keyMemory] = [];
+    }
+
+    creep.memory.um[keyMemory].push(valueMemory);
+  },
+  forgetInArray(creep, keyMemory, valueMemory) {
+    if (creep.memory.um == undefined) {
+      creep.memory.um = {};
+    }
+
+    if (creep.memory.um[keyMemory] === undefined) {
+      creep.memory.um[keyMemory] = [];
+    }
+
+    if (creep.memory.um[keyMemory].contains[valueMemory]) {
+      console.log("forgetting in array. " + creep + " " + keyMemory + " " + valueMemory);
+    }
+  },
+  forgetInArrayByIndex(creep, keyMemory, index) {
+    if (creep.memory.um == undefined) {
+      creep.memory.um = {};
+    }
+
+    if (creep.memory.um[keyMemory] === undefined) {
+      creep.memory.um[keyMemory] = [];
+    }
+
+    var valueMemory = creep.memory.um[keyMemory][index];
+
+    this.forgetInArray(creep, keyMemory, valueMemory);
   }
 };
 

719ee947e1f22e491aedb2ee1ad90a0af28c0cce
Chris Connelley <chris.connelley@divvypay.com>
2019-11-01 19:00:00 -0600

    new roles

--- brain.creeps.js
+++ brain.creeps.js
@@ -2,6 +2,11 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleLoader = require('role.loader');
+var roleExcavator = require('role.excavator');
+var roleTransport = require('role.transport');
+var roleScout = require('role.scout');
+var roleRefueler = require('role.refueler');
+var roleBattery = require('role.battery');
 
 var brainCreeps = {
   run: function() {
@@ -18,8 +23,24 @@ var brainCreeps = {
       }
       if(creep.memory.role == 'gatherer') {
         roleHarvester.run(creep);
-      }if(creep.memory.role == 'loader') {
+      }
+      if(creep.memory.role == 'loader') {
         roleLoader.run(creep);
+      }
+      if(creep.memory.role == 'excavator') {
+        roleExcavator.run(creep);
+      }
+      if(creep.memory.role == 'transport') {
+        roleTransport.run(creep);
+      }
+      if(creep.memory.role == 'scout') {
+        roleScout.run(creep);
+      }
+      if(creep.memory.role == 'refueler') {
+        roleRefueler.run(creep);
+      }
+      if(creep.memory.role == 'battery') {
+          roleBattery.run(creep);
       }
   }
   }
